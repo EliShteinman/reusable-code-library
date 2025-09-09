2e7ae0c680a8b9794b3dfe3410c583ba0327fd41
a0533 <7198754@gmail.com>
2025-09-09 10:09:16 +0300

    Refactor `whisper.py` for enhanced transcription functionality
    - Adjusted `whisper_transcribe` method to support additional arguments via `**kwargs`.
    - Added a main execution block for standalone usage and debugging.
    - Improved parameter formatting and code readability.
    - Introduced sample usage with `pprint` for showcasing transcription outputs.

--- shared-utilities/stt/whisper.py
+++ shared-utilities/stt/whisper.py
@@ -1,23 +1,28 @@
-import whisper
 import logging
-from datetime import datetime
-from elasticsearch import Elasticsearch
+
+import whisper
 
 logger = logging.getLogger(__name__)
 
 
 class WhisperService:
-    def __init__(self, model_name: str, download_root: str):
+    def __init__(
+            self,
+            model_name: str,
+            download_root: str
+    ):
         self.model = whisper.load_model(
             name=model_name,
             download_root=download_root
         )
 
-    def whisper_transcribe(self, file_path, file_hash: str):
-        """תמלול מלא - מחזיר כל המידע הרלוונטי"""
-        result = self.model.transcribe(
-            file_path,
-            word_timestamps=True
+    def whisper_transcribe(self, file_path, file_hash: str, **kwargs):
+        result = whisper.transcribe(
+            model=self.model,
+            audio=file_path,
+            fp16=False,
+            word_timestamps=True,
+            **kwargs
         )
 
         logger.info(f"Detected language: {result['language']}")
@@ -29,6 +34,20 @@ class WhisperService:
             'language': result['language'],
             'segments': result['segments']
         }
+
+
+if __name__ == "__main__":
+    from pprint import pprint
+    whisper_service = WhisperService(
+        model_name="tiny",
+        download_root=r"C:\models\whisper"
+    )
+
+    result = whisper_service.whisper_transcribe(
+            r"C:\podcasts\download (18).wav",
+            "jhgyuftydtuyytftrdtr"
+    )
+    pprint(result['full_text'])
 
 
 def save_transcription_to_elasticsearch(transcription, es_client, index_name="transcriptions"):
